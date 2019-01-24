2f58dec445e259d91b78fe447ad784eebbb2b7e4
visualstudio <visualstudio@230-PC30>
2019-01-24 16:50:25 -0500

    commit maked

---
+++ 730462/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _730462
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
